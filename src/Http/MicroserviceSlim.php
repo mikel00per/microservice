c4bda1fdc12a72642c4b47f93f179c2d4a4d5b5c
Miguel Morillo <miguelmwilliams@gmail.com>
2022-11-13 16:55:19 +0100

    feat: added microservice slim class

---
+++ src/Http/MicroserviceSlim.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Microservices\Http;
+
+use ContainerSettings\SettingsInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Slim\App;
+
+class MicroserviceSlim extends App implements MicroserviceSlimInterface
+{
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct(
+            $container->get(ResponseFactoryInterface ::class),
+            $container
+        );
+
+        $settings = $container->get(SettingsInterface::class);
+
+        if ($settings->get('router.cache.enabled')) {
+            $this->getRouteCollector()->setCacheFile($settings->get('router.cache.path'));
+        }
+    }
+}
---
+++ src/Http/MicroserviceSlimInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Microservices\Http;
+
+use Psr\Container\ContainerInterface;
+use Slim\Interfaces\RouteCollectorProxyInterface;
+
+interface MicroserviceSlimInterface extends RouteCollectorProxyInterface
+{
+    public function __construct(ContainerInterface $container);
+}
