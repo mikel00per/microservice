9e8624d1d63c93c6d7af1680bfb2c5f0c8a3d567
Miguel Morillo <miguelmwilliams@gmail.com>
2022-11-13 17:01:39 +0100

    feat: added default middlewares

---
+++ src/Http/Middlewares/DefaultHeadersMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Microservices\Http\Middlewares;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class DefaultHeadersMiddleware implements Middleware
+{
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        return $handler->handle($request)
+            ->withHeader('Content-Type', 'application/json')
+            ->withHeader('Connection', 'close')
+            ->withProtocolVersion('1.0');
+    }
+}
