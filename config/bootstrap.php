935550f38ceaf437049c5dca38b0f42b049dec7d
Miguel Morillo <miguelmwilliams@gmail.com>
2023-11-27 19:54:27 +0100

    improvement(config): change settings structure to need it work with container settings

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,15 +2,16 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Shared\Infrastructure\DependencyInjection\ContainerFactory;
 use Shared\Infrastructure\Slim\MicroserviceSlimInterface;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
-    $container = ContainerFactory::buildContainer(__DIR__ . '/../config/settings.php');
-
-    return $container->get(MicroserviceSlimInterface::class);
-} catch (Exception $e) {
+    return ContainerFactory::create(__DIR__ . '/../config/settings.php')
+        ->get(MicroserviceSlimInterface::class);
+} catch (NotFoundExceptionInterface|ContainerExceptionInterface|Exception $e) {
     throw new RuntimeException('Cant start');
 }
